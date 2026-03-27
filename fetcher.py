cd0fdc923421a9dfb3d305969852ce22de773bca
kashish6624 <156503728+kashish6624@users.noreply.github.com>
2026-03-27 23:36:02 +0530

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -1,7 +1,20 @@
 from newspaper import Article
+from newspaper.article import ArticleException
+
 
 def fetch_article_from_url(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    return article.text
+    try:
+        article = Article(url)
+
+        article.download()
+        article.parse()
+
+        return article.text
+
+    except ArticleException as e:
+        print("Download blocked:", e)
+        return ""
+
+    except Exception as e:
+        print("Unexpected error:", e)
+        return ""
